74b31d20a44b2b14e97d54c9e0673d21dcccf8b1
divinomarciok <58217165+divinomarciok@users.noreply.github.com>
2025-03-28 00:28:24 -0300

    Foi realizada a criação do service que recebe os ojetos em formato json e serializada para objetos pricelistID

--- front_leilaorv/lib/screens/home.dart
+++ front_leilaorv/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:front_leilaorv/service/fetchEnterprises.dart';
+import 'package:front_leilaorv/service/fetchPriceList.dart';
 import '../models/enterprise.dart';
 
 class leilaoHome extends StatefulWidget {
@@ -9,61 +10,62 @@ class leilaoHome extends StatefulWidget {
 }
 
 class _leilaoHomeState extends State<leilaoHome> {
-
   late Future<dynamic> _enterprisesFuture;
-  final EnterpriseService _service = EnterpriseService();
+  final PriceListService _service = PriceListService();
+  late int id = 3;
+  late String authorization =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibG9naW4iOiJhY2Vyb2xhIiwiaWF0IjoxNzQzMTMwNTcyLCJleHAiOjE3NDMxMzQxNzJ9.OlutSdqenxh3c11eUFZrhocLNV3mHUSoAc3baIK9ZMU";
 
   @override
   void initState() {
     super.initState();
-    _enterprisesFuture = _service.getAllEnterprise();
+    _enterprisesFuture = _service.getAllPriceListId(id, authorization);
   }
-  
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Leilão Rio Verde"),
-          backgroundColor: Colors.lightGreen,
-        ),
-        body: Container(
-          height: 400,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            padding: const EdgeInsets.all(2),
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: const EdgeInsets.only(right: 10),
-                width: 200,
-                child: Card(
-                  elevation: 4,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Image.network(
-                          'https://i1.wp.com/qrofertas.s3.us-west-2.amazonaws.com/imagens-ads/d0691886-4ea4-4e5b-bc76-ca1a9e680b38-modelo-de-encarte-4-tema-4159-pagina-1-art-82761.jpg?w=582',
-                          height: 280,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
 
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Produto $index+1 \nR\$ 100,00 \nCategoria : Congelados \nMarca : Boa ",
-                        ),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Leilão Rio Verde"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Container(
+        height: 400,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.all(2),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: const EdgeInsets.only(right: 10),
+              width: 200,
+              child: Card(
+                elevation: 4,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Image.network(
+                        'https://i1.wp.com/qrofertas.s3.us-west-2.amazonaws.com/imagens-ads/d0691886-4ea4-4e5b-bc76-ca1a9e680b38-modelo-de-encarte-4-tema-4159-pagina-1-art-82761.jpg?w=582',
+                        height: 280,
+                        fit: BoxFit.cover,
                       ),
-                    ],
-                  ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Produto $index+1 \nR\$ 100,00 \nCategoria : Congelados \nMarca : Boa ",
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
-          ),
+              ),
+            );
+          },
         ),
-      );
-    }
+      ),
+    );
   }
-
+}
